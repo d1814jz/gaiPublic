ac2904282ea649178ed53b1e3af7c88bd0ac6b44
d1814jz <124612063+d1814jz@users.noreply.github.com>
2023-02-07 11:27:33 +0300

    Add files via upload

---
+++ Accounting/AccountingFormNew.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace gai
+{
+    partial class AccountingFormNew
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AccountingFormNew));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnGridView = new System.Windows.Forms.Button();
+            this.bntAuto = new System.Windows.Forms.Button();
+            this.bntDrivers = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DateCheckBox = new System.Windows.Forms.CheckBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bntDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(950, 513);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
+            // 
+            // btnGridView
+            // 
+            this.btnGridView.Location = new System.Drawing.Point(984, 237);
+            this.btnGridView.Name = "btnGridView";
+            this.btnGridView.Size = new System.Drawing.Size(128, 40);
+            this.btnGridView.TabIndex = 1;
+            this.btnGridView.Text = "Отобразить";
+            this.btnGridView.UseVisualStyleBackColor = true;
+            this.btnGridView.Click += new System.EventHandler(this.btnGridView_Click);
+            // 
+            // bntAuto
+            // 
+            this.bntAuto.Location = new System.Drawing.Point(984, 12);
+            this.bntAuto.Name = "bntAuto";
+            this.bntAuto.Size = new System.Drawing.Size(128, 42);
+            this.bntAuto.TabIndex = 2;
+            this.bntAuto.Text = "Автомобили";
+            this.bntAuto.UseVisualStyleBackColor = true;
+            this.bntAuto.Click += new System.EventHandler(this.bntAuto_Click);
+            // 
+            // bntDrivers
+            // 
+            this.bntDrivers.Location = new System.Drawing.Point(984, 69);
+            this.bntDrivers.Name = "bntDrivers";
+            this.bntDrivers.Size = new System.Drawing.Size(128, 51);
+            this.bntDrivers.TabIndex = 5;
+            this.bntDrivers.Text = "Водители";
+            this.bntDrivers.UseVisualStyleBackColor = true;
+            this.bntDrivers.Click += new System.EventHandler(this.bntDrivers_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(968, 39);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 27);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(965, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Поиск";
+            // 
+            // DateCheckBox
+            // 
+            this.DateCheckBox.AutoSize = true;
+            this.DateCheckBox.Location = new System.Drawing.Point(1051, 12);
+            this.DateCheckBox.Name = "DateCheckBox";
+            this.DateCheckBox.Size = new System.Drawing.Size(64, 21);
+            this.DateCheckBox.TabIndex = 9;
+            this.DateCheckBox.Text = "Дата";
+            this.DateCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(984, 485);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(137, 40);
+            this.btnBack.TabIndex = 10;
+            this.btnBack.Text = "Назад";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(984, 131);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(128, 51);
+            this.btnAdd.TabIndex = 11;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(649, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 12;
+            // 
+            // bntDelete
+            // 
+            this.bntDelete.Location = new System.Drawing.Point(984, 198);
+            this.bntDelete.Name = "bntDelete";
+            this.bntDelete.Size = new System.Drawing.Size(128, 48);
+            this.bntDelete.TabIndex = 13;
+            this.bntDelete.Text = "Снять с учета";
+            this.bntDelete.UseVisualStyleBackColor = true;
+            this.bntDelete.Click += new System.EventHandler(this.bntDelete_Click);
+            // 
+            // AccountingFormNew
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1127, 528);
+            this.Controls.Add(this.bntDelete);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.DateCheckBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bntDrivers);
+            this.Controls.Add(this.bntAuto);
+            this.Controls.Add(this.btnGridView);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AccountingFormNew";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AccountingFormNew";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnGridView;
+        private System.Windows.Forms.Button bntAuto;
+        private System.Windows.Forms.Button bntDrivers;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox DateCheckBox;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button bntDelete;
+    }
+}
